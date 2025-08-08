9a3d3cf6010f975c689fbe0a46baf8bbf841b22c
leechwork3-pixel <leechwork3@gmail.com>
2025-08-08 12:34:51 +0530

    Update antinsfw.py

--- plugins/antinsfw.py
+++ plugins/antinsfw.py
@@ -1,27 +1,71 @@
-nsfw_keywords = [
-    "porn", "sex", "nude", "naked", "fuck", "boobs", "xxx", "pussy", "dick", "ass", 
-    "cum", "bdsm", "shemale", "fetish", "milf", "hentai", "18+", "loli", "rape",
-    "erotic", "lewd", "tramp", "creampie", "tits", "orgasm", "vagina", "clitoris",
-    "pedo", "bondage", "masturbate", "masturbation"
-]
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
-exception_list = ["assassin", "assault", "classroom", "geass"]
+# NSFW keyword categories
+nsfw_keywords = {
+    "general": [
+        # (same as your provided list)
+        "porn", "sex", "nude", "naked", "boobs", "tits", "pussy", "dick", "cock", "ass",
+        "fuck", "blowjob", "cum", "orgasm", "shemale", "erotic", "masturbate", "anal",
+        "hardcore", "bdsm", "fetish", "lingerie", "xxx", "milf", "gay", "lesbian",
+        "threesome", "squirting", "butt plug", "dildo", "vibrator", "escort", "handjob",
+        "striptease", "kinky", "pornstar", "sex tape", "spank", "swinger", "taboo", "cumshot",
+        "deepthroat", "domination", "submission", "handcuffs", "orgy", "roleplay", "sex toy",
+        "voyeur", "cosplay", "adult", "culture", "pornhwa", "netorare", "netori", "netorase",
+        "eromanga", "incest", "stepmom", "stepdad", "stepsister", "stepbrother", "stepson",
+        "stepdaughter", "ntr", "gangbang", "facial", "golden shower", "pegging", "rimming",
+        "rough sex", "dirty talk", "sex chat", "nude pic", "lewd", "titty", "twerk", "breasts",
+        "penis", "vagina", "clitoris", "genitals", "sexual", "kamasutra", "pedo", "rape",
+        "bondage", "cum inside", "creampie", "sex slave", "sex doll", "sex machine", "latex",
+        "oral sex", "butt", "slut", "whore", "tramp", "skank", "cumdumpster", "cultured",
+        "ecchi", "doujin", "hentai", "smut", "waifu", "futanari", "tentacle"
+    ],
+    "hentai": [
+        "hentai", "doujinshi", "ecchi", "yaoi", "shota", "loli", "tentacle", "futanari",
+        "bishoujo", "bishounen", "mecha hentai", "hentai manga", "hentai anime", "smut",
+        "eroge", "visual novel", "h-manga", "h-anime", "adult manga", "18+ anime", "18+ manga",
+        "lewd anime", "lewd manga", "animated porn", "animated sex", "hentai game", "hentai art",
+        "hentai drawing", "hentai doujin", "yaoi hentai", "hentai comic", "hentai picture",
+        "hentai scene", "hentai story", "hentai video", "hentai movie", "hentai episode", "hentai series"
+    ],
+    "abbreviations": [
+        "pr0n", "s3x", "n00d", "fck", "bj", "hj", "l33t", "p0rn", "h3ntai", "h-ntai", "pnwh",
+        "p0rnhwa", "l33tsp34k", "l3wd", "cultur3d", "s3xual"
+    ],
+    "offensive_slang": [
+        "slut", "whore", "tramp", "skank", "cumdumpster", "gangbang", "facial", "golden shower",
+        "pegging", "rimming", "rough sex", "dirty talk", "sex chat", "nude pic", "lewd", "titty",
+        "twerk", "breasts", "penis", "vagina", "clitoris", "genitals", "sexual", "kamasutra",
+        "incest", "pedo", "rape", "sex slave", "bondage", "creampie", "cum inside", "sex doll",
+        "sex machine", "latex", "oral sex", "cumshot", "deepthroat", "domination", "submission",
+        "handcuffs", "orgy", "roleplay", "sex toy", "voyeur", "cosplay", "adult", "culture",
+        "anal", "erotic", "masturbate", "hardcore", "bdsm", "fetish", "lingerie", "milf", "taboo"
+    ]
+}
 
+# List of safe exceptions (useful terms that shouldn't trigger false positive matches)
+exception_keywords = ["nxivm", "classroom", "assassination", "geass"]
 
-async def check_anti_nsfw(file_name: str, message):
+async def check_anti_nsfw(file_name: str, message: Message) -> bool:
+    """
+    Checks a file name for NSFW terms. Returns True if blocked.
+    """
     if not file_name:
         return False
 
-    lower = file_name.lower()
+    name_lower = file_name.lower()
 
-    for exception in exception_list:
-        if exception in lower:
-            return False
+    # Check exceptions first
+    for safe in exception_keywords:
+        if safe in name_lower:
+            return False  # Allow it
 
-    for word in nsfw_keywords:
-        if word in lower:
-            await message.reply_text("🚫 File not allowed due to NSFW content.")
-            return True
+    # Run NSFW keyword checks
+    for category, keywords in nsfw_keywords.items():
+        for kw in keywords:
+            if kw in name_lower:
+                await message.reply_text("🚫 NSFW content detected! Renaming of this file is not allowed.")
+                return True
 
     return False
     
