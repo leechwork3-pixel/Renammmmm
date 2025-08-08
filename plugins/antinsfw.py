f8d1635d42d82702c0c3d81f7be10181726752b5
leechwork3-pixel <leechwork3@gmail.com>
2025-08-08 12:25:05 +0530

    Update antinsfw.py

--- plugins/antinsfw.py
+++ plugins/antinsfw.py
@@ -1,11 +1,27 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
+nsfw_keywords = [
+    "porn", "sex", "nude", "naked", "fuck", "boobs", "xxx", "pussy", "dick", "ass", 
+    "cum", "bdsm", "shemale", "fetish", "milf", "hentai", "18+", "loli", "rape",
+    "erotic", "lewd", "tramp", "creampie", "tits", "orgasm", "vagina", "clitoris",
+    "pedo", "bondage", "masturbate", "masturbation"
+]
 
-BANNED_KEYWORDS = ["porn", "xvideos", "nude", "sex", "18+"]
+exception_list = ["assassin", "assault", "classroom", "geass"]
 
-@Client.on_message(filters.private & filters.media)
-async def nsfw_check(client, message: Message):
-    filename = message.document.file_name if message.document else ""
-    if any(x.lower() in filename.lower() for x in BANNED_KEYWORDS):
-        return await message.reply_text("🚫 NSFW detected. File not allowed.")
-      
+
+async def check_anti_nsfw(file_name: str, message):
+    if not file_name:
+        return False
+
+    lower = file_name.lower()
+
+    for exception in exception_list:
+        if exception in lower:
+            return False
+
+    for word in nsfw_keywords:
+        if word in lower:
+            await message.reply_text("🚫 File not allowed due to NSFW content.")
+            return True
+
+    return False
+    
