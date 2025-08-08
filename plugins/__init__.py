534f96b53d09a5c0fef68c87112a834869a62def
leechwork3-pixel <leechwork3@gmail.com>
2025-08-08 16:14:17 +0530

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,5 @@
+# This file allows Pyrogram to treat the 'plugins' folder as a package.
+# All .py files in here will be auto-loaded by Pyrogram because
+# bot.py uses plugins={"root": "plugins"}.
+
+# You can also put global variables or helper imports here if desired.
